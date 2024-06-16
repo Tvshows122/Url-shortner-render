933c80dada7afb7da4f174a4ff9b780d7b783404
Tvshows122 <126122240+Tvshows122@users.noreply.github.com>
2024-06-16 10:51:54 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $SOURCE_CODE ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Tvshows122/URL-Shortener-V2
+  git clone https://github.com/Tvshows122/URL-Shortener-V2.git /URL
 else
   echo "Cloning Custom Repo from $SOURCE_CODE "
-  git clone $SOURCE_CODE /URL-Shortener
+  git clone $SOURCE_CODE /URL
 fi
-cd /URL-Shortener
+cd /URL
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
