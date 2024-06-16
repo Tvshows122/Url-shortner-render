97da3297ced3710be013a3bb0aa4ed4d7a963e6a
Tvshows122 <126122240+Tvshows122@users.noreply.github.com>
2024-06-16 11:27:40 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -21,7 +21,6 @@ from utils import extract_link, get_me_button, get_size
 logger = logging.getLogger(__name__)
 
 user_commands = [
-    "mdisk_api",
     "shortener_api",
     "header",
     "footer",
@@ -31,15 +30,7 @@ user_commands = [
     "me",
 ]
 avl_web = [
-    "droplink.co",
-    "gplinks.in",
-    "tnlink.in",
-    "za.gl",
-    "du-link.in",
-    "viplink.in",
-    "shorturllink.in",
-    "shareus.in",
-    "earnspace.in",
+"bmlinks.com"
 ]
 
 avl_web1 = "".join(f"- {i}\n" for i in avl_web)
@@ -85,7 +76,7 @@ async def help_command(c, m: Message):
         firstname=temp.FIRST_NAME,
         username=temp.BOT_USERNAME,
         repo=SOURCE_CODE,
-        owner="@ask_admin001",
+        owner="@Bmlinks_Support_bot",
     )
 
     if WELCOME_IMAGE:
