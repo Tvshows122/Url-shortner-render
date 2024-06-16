0a3ca38f966565c32208dca8e08c9c6221d7853d
Tvshows122 <126122240+Tvshows122@users.noreply.github.com>
2024-06-16 10:43:00 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ then
   git clone https://github.com/Tvshows122/URL-Shortener-V2
 else
   echo "Cloning Custom Repo from $SOURCE_CODE "
-  git clone $SOURCE_CODE /URL-Shortener-V2
+  git clone $SOURCE_CODE /URL-Shortener
 fi
 cd /URL-Shortener-V2
 pip3 install -U -r requirements.txt
